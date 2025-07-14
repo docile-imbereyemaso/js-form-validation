b1092deeedeb467b8dcdf67806ccac74386a85fe
Docile123 <imbereyemasoo@gmail.com>
2025-07-14 12:30:25 +0200

    update the js file

---
+++ validation.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("#form");
+const firstname = document.querySelector("#firstname-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
+const repeatPasswordInput  = document.querySelector("#repeat-password-input");
+
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+})
